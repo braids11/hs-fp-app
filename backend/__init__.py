99b06a1cd3bc5ce0840608dac0549ee42d18f578
Mike Brady <32839439+braids11@users.noreply.github.com>
2017-10-30 15:18:15 +0000

    Home Assignment corrected

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,6 +13,7 @@ def process_user_query(query_string):
     for i in names:
         if i.istitle():
             greetings.append(f'Hi {i}!')
+    greetings = set(greetings)
     return greetings
 
 #print(process_user_query("Alex Alex went to Mike dinner with Animesh"))
