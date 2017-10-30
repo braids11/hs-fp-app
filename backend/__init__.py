9d5f73f15e1ab0a01fcda5baad3bedce46fbfa7a
Mike Brady <32839439+braids11@users.noreply.github.com>
2017-10-30 15:10:16 +0000

    Unique names

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,19 @@
+#def process_user_query(query_string):
+#    names = query_string.split()
+#    greetings = []
+#    for i in names:
+#        greetings.append(f'Hi {i}')
+#    return greetings
+
+# print(process_user_query("Alex Bob Mike"))
+
 def process_user_query(query_string):
-    names = query_string.split(' ')
+    names = query_string.split()
     greetings = []
     for i in names:
-        greetings.append(f'Hi {i}')
-        return greetings
+        if i.istitle():
+            greetings.append(f'Hi {i}!')
+    greetings = set(greetings)
+    return greetings
 
-print(process_user_query("Alex Bob"))
+# print(process_user_query("Alex Alex went to dinner with Animesh"))
